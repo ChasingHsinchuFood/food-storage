f3b539ee6ccbe774cf79055c676328b391f2d86a
peter279k <peter279k@gmail.com>
2017-11-25 10:25:07 +0800

    add fgets string

--- main.php
+++ main.php
@@ -38,14 +38,23 @@ $storage->createSchema($capsule);
 
 $foodInfo = $storage::Create([]);
 
-$resource = $storage->getCsvContent('stream', './db.shop.csv');
+$resource = $storage->getCsvContent('stream', '../food-crawler/db.shop.csv');
 
-var_dump($foodInfo->foodInfo()->create([
-    'address' => $address,
-    'phone_number' => $phoneNumber,
-    'rate' => $rate,
-    'shop_name' => $shopName,
-    'static_map_image' => $mapImage,
-]));
+while(!feof($resource)) {
+    $string = explode(',', fgets($resource, 4096));
+    $address = $string[0];
+    $phoneNumber = $string[1];
+    $rate = $string[2];
+    $shopName = $string[3];
+    $mapImage = $string[4];
+
+    var_dump($foodInfo->foodInfo()->create([
+        'address' => $address,
+        'phone_number' => $phoneNumber,
+        'rate' => $rate,
+        'shop_name' => $shopName,
+        'static_map_image' => $mapImage,
+    ]));
+}
 
 $storage->closeStream();
